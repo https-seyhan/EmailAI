d32fd3ffddff8daadc5d7e74b4cffef2d2cf7179
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2022-05-17 22:36:43 +1000

    Add files via upload

--- email_reads.py
+++ email_reads.py
@@ -4,12 +4,14 @@ import psutil
 import datetime
 import json
 import eml_parser
-#read email context
+
 class readFiles:
 	name= 'read email files'
 	print('Read Files')
+
+
 	def __init__(self):
-		
+		#self.read_memory()
 		#print(psutil.virtual_memory())
 		print('Virtual Memory Percent ', psutil.virtual_memory().percent)
 		print(psutil)
@@ -21,7 +23,11 @@ class readFiles:
   			raw_email = email.read()
 		ep = eml_parser.EmlParser()
 		parsed_eml = ep.decode_email_bytes(raw_email)
-		print(json.dumps(parsed_eml))
+		print(json.dumps(parsed_eml, indent=4, sort_keys=True, default=str))
+		self.get_information(json.dumps(parsed_eml, indent=4, sort_keys=True, default=str))
+	
+	def get_information(self, text):
+		print('Email Text ', text)
 
 
 
