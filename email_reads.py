d4554d82b10a6f99408e63e6a418563f01784a61
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2022-05-23 19:44:55 +1000

    Add files via upload

--- email_reads.py
+++ email_reads.py
@@ -3,13 +3,17 @@ import os
 import psutil
 import datetime
 import json
-import eml_parser #email module
+import eml_parser
 
 class readFiles:
 	name= 'read email files'
 	print('Read Files')
+
+
 	def __init__(self):
 		self.read_eml_file()
+
+
 	def read_eml_file(self):
 		with open('sample_file.eml', 'rb') as email:
   			raw_email = email.read()
@@ -17,11 +21,17 @@ class readFiles:
 		parsed_eml = ep.decode_email_bytes(raw_email)
 		#print(json.dumps(parsed_eml))
 		self.get_information(json.dumps(parsed_eml, indent=4, sort_keys=True, default=str))
+		
 	
 	def get_information(self, text):
-		#print('Email Text ', text)
+		print('Email Text ', text)
 		print(type(text))
+		#json.load(text)
+
 		
+
+
+
 #files = readFiles()
 readFiles()
 #files
