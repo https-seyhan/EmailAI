2236a9561cc211e17635a506bc7414b6dfce05be
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2022-05-24 21:04:31 +1000

    Add files via upload

--- email_reads.py
+++ email_reads.py
@@ -9,23 +9,29 @@ class readFiles:
 	name= 'read email files'
 	print('Read Files')
 
+
 	def __init__(self):
 		self.read_eml_file()
+
+
 	def read_eml_file(self):
 		with open('sample_file.eml', 'rb') as email:
   			raw_email = email.read()
-
 		ep = eml_parser.EmlParser()
 		parsed_eml = ep.decode_email_bytes(raw_email)
 		#print(json.dumps(parsed_eml))
 		self.get_information(json.dumps(parsed_eml, indent=4, sort_keys=True, default=str))
-
+		
+	
 	def get_information(self, text):
 		#print('Email Text ', text)
 		print(type(text))
 		json_data = json.loads(text)
-
-
+		
+		
+		#for item in json_data:
+		#	print( "Item ", item)
+		
 		#print(json_data['body'])
 		#print(json_data['header'])
 		self.get_header(json_data)
@@ -47,8 +53,7 @@ class readFiles:
 			#print(item['date'])
 			if item == 'received':
 				print('Received called')
-				for key in item:
-					print(key)
+				print(json_object['header']['received'][0])
 
 
 
