4bc1aab7aefee7967e75d4a6662ffb5be268bd44
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2022-05-25 19:34:35 +1000

    Add files via upload

--- email_reads.py
+++ email_reads.py
@@ -12,6 +12,7 @@ class readFiles:
 	def __init__(self):
 		self.read_eml_file()
 
+
 	def read_eml_file(self):
 		with open('sample_file.eml', 'rb') as email:
   			raw_email = email.read()
@@ -19,16 +20,18 @@ class readFiles:
 		parsed_eml = ep.decode_email_bytes(raw_email)
 		#print(json.dumps(parsed_eml))
 		self.get_information(json.dumps(parsed_eml, indent=4, sort_keys=True, default=str))
-
+		
+	
 	def get_information(self, text):
 		#print('Email Text ', text)
 		#print(type(text))
 		json_data = json.loads(text)
-
+		
+		
 		#print(json_data['body'])
 		#print(json_data['header'])
 		self.get_header(json_data)
-
+		
 	def get_header(self, json_object):
 		#print('Date ', json_object['header']['date'], '\n')
 		#print('From ', json_object['header']['from'], '\n')
@@ -39,18 +42,21 @@ class readFiles:
 		#print('Received IP', json_object['header']['received_ip'], '\n')
 		#print('Subject ', json_object['header']['subject'], '\n')
 		#print('To ', json_object['header']['to'], '\n')
-
+		
 		for item in json_object['header']:
 			#print(item)
 			#print(item['date'])
 			if item == 'received':
 				print('Received called')
 				#print(len(json_object['header']['received']))
-
+				
 				for element in range(len(json_object['header']['received'])):
 					print('Received elements ', json_object['header']['received'][element])
 					elements = list(json_object['header']['received'][element].items())
 					readFiles.email_details.append(elements)
+
+
+
 #files = readFiles()
 readFiles()
 #files
