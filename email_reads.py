a1b62e67d6d69a84b591e63afef5347533a83374
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2022-05-28 08:33:05 +1000

    Update email_reads.py

--- email_reads.py
+++ email_reads.py
@@ -19,7 +19,7 @@ class readFiles:
 		with open('sample_file.eml', 'rb') as email:
   			raw_email = email.read()
 		ep = eml_parser.EmlParser()
-		
+	
 		parsed_eml = ep.decode_email_bytes(raw_email)
 		#print(json.dumps(parsed_eml))
 		self.get_information(json.dumps(parsed_eml, indent=4, sort_keys=True, default=str))
