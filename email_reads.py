ef66f945cc781a813e35dbfc438a3acc4c293828
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2022-06-20 20:17:45 +1000

    Add files via upload

--- email_reads.py
+++ email_reads.py
@@ -10,31 +10,32 @@ os.chdir('/home/saul/kaggle')
 class readFiles:
 	name= 'read email files'
 	email_details = []
-	def __init__(self):
 
+	def __init__(self):
 		self.email_context = [['', '','', '', '', '']]
 		self.read_eml_file()
+
 
 	def read_eml_file(self):
 		with open('sample_file.eml', 'rb') as email:
   			raw_email = email.read()
-
 		ep = eml_parser.EmlParser()
 		parsed_eml = ep.decode_email_bytes(raw_email)
 		#print(json.dumps(parsed_eml))
 		self.get_information(json.dumps(parsed_eml, indent=4, sort_keys=True, default=str))
-
+		
+	
 	def get_information(self, text):
 		#print('Email Text ', text)
 		#print(type(text))
 		json_data = json.loads(text)
-
+		
+		
 		#print(json_data['body'])
 		#print(json_data['header'])
 		self.get_header(json_data)
-
-	def get_header(self, json_object):
 		
+	def get_header(self, json_object):
 		#print('Date ', json_object['header']['date'], '\n')
 		#print('From ', json_object['header']['from'], '\n')
 		#print('Received ', json_object['header']['received'], '\n')
